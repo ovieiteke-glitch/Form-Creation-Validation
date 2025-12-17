95549687436557e930c3a8b83228490ccb7db136
Ovie Iteke <ovie.iteke@gmail.com>
2025-12-17 17:09:43 +0100

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,1 +1,9 @@
-fetchUserData
+//Initializing the Async Function
+function fetchUserdata() {
+    //Defining the API URL
+    const apiurl = "https://jsonplaceholder.typicode.com/users";
+    //Selecting the Data Container Element
+    dataContainer = document.getElementById("api-data").html;
+    //Fetch Data Using try-catch:
+    
+}
